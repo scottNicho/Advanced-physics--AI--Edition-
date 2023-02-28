ddb723f706af18e9ad2a12f14c0a0124426b4c15
scottNicho <120742244+scottNicho@users.noreply.github.com>
2023-02-28 16:15:33 +0000

    Update playerState.h
    moved initialisation and main body of main functions into cpp file

--- playerState.h
+++ playerState.h
@@ -14,13 +14,7 @@ namespace NCL::CSC8503 {
 	class playerState :public GameObject {
 	public:
 
-		playerState(GameObject* theEnemy) {
-			playerForwardState = still;
-			playerSideState = still_side;
-			playerTrack = nullptr;
-			enemy = theEnemy;
-
-		};
+		playerState(GameObject* theEnemy);
 
 		struct PositionData
 		{
@@ -30,6 +24,8 @@ namespace NCL::CSC8503 {
 			float distanceFromCentre;
 
 		};
+
+
 		
 		enum currentStateForward
 		{
@@ -53,89 +49,30 @@ namespace NCL::CSC8503 {
 
 		};
 
+		struct totalState
+		{
+			currentStateForward forwardResponse;
+			currentStateSideward sidewardsResponse;
+		};
 
-		void setCurrentPosition() {
-			PreviousPosition = currentPosition;
-			Vector3 playerPos = this->GetTransform().GetPosition();
-			Vector3 playerEnemyPos = playerPos - (enemy->GetTransform().GetPosition());
-			float PlayerDistEnemy = playerEnemyPos.Length();
-			float PlayerDistCentre = (playerPos - Vector3{ 0,-20,0 }).Length();
-			currentPosition = PositionData{ playerPos,playerEnemyPos,PlayerDistEnemy,PlayerDistCentre };
-		}
 
-		void setIsPlayerJumping() {
-			if (currentPosition.playerPosition.y > PreviousPosition.playerPosition.y && currentPosition.playerPosition.y > 1) {
-				playerIsJumping = true;
-				return;
-			}
-			playerIsJumping = false;
-		}
+		void updateResponse();
 
-		bool getPlayerIsJumping() {
+		void setCurrentPosition();
+
+		void setIsPlayerJumping();
+
+		bool getIsPlayerJumping() {
 			return playerIsJumping;
 		}
 
-		void updatePlayerForwardState() {
-			float changeInDistance = (currentPosition.distanceFromEnemy) - (PreviousPosition.distanceFromEnemy);
-			if (abs(changeInDistance) < 3) {
-				playerForwardState = still;
-				return;
-			}
-			if (changeInDistance >= 3) {
-				playerForwardState = backwards;
-				return;
-			}
-			else
-			{
-				playerForwardState = forwards;
-				return;
-			}
-			
-		}
+		void updatePlayerForwardState();
 
-		void updatePlayerForwardStateJumping() {
-			float changeInDistance = (currentPosition.distanceFromEnemy) - (PreviousPosition.distanceFromEnemy);
-			if (abs(changeInDistance) < 3) {
-				playerForwardState = jumping;
-				return;
-			}
-			if (changeInDistance >= 3) {
-				playerForwardState = jumping_backwards;
-				return;
-			}
-			else
-			{
-				playerForwardState = jumping_forwards;
-				return;
-			}
+		void updatePlayerForwardStateJumping();
 
-		}
+		void updatePlayerSideState();
 
-		void updatePlayerSideState() {
-			float AnglePreviousCurrent = Vector3::Dot(PreviousPosition.relativePosition, currentPosition.relativePosition);//the angle between the players 2 positions relative to teh player
-			if (AnglePreviousCurrent > 5) {
-				playerSideState = run_right;
-				return;
-			}
-			if (AnglePreviousCurrent < 5) {
-				playerSideState = run_left;
-				return;
-			}
-			playerSideState = still_side;
-		}
-
-		void updatePlayerSideStateJumping() {
-			float AnglePreviousCurrent = Vector3::Dot(PreviousPosition.relativePosition, currentPosition.relativePosition);//the angle between the players 2 positions relative to teh player
-			if (AnglePreviousCurrent > 5) {
-				playerSideState = jumping_right;
-				return;
-			}
-			if (AnglePreviousCurrent < 5) {
-				playerSideState = jumping_left;
-				return;
-			}
-			playerSideState = still_side;
-		}
+		void updatePlayerSideStateJumping();
 
 		void setPlayerTrack(GameObject* thePlayer) {
 			playerTrack = thePlayer;
@@ -161,9 +98,7 @@ namespace NCL::CSC8503 {
 			playerSideState = newState;
 		}
 
-		void updateState() {
-
-		}
+		void updateState();
 
 	protected:
 
@@ -174,6 +109,38 @@ namespace NCL::CSC8503 {
 		GameObject* enemy;
 		PositionData currentPosition;// compare these 2 to set the new state 
 		PositionData PreviousPosition;
+		set<vector <totalState>> totalResponse;
+		totalState response{};
 
+
+		vector <totalState> still_stillSide;
+		vector <totalState> still_runRight;
+		vector <totalState> still_runLeft;
+		vector <totalState> still_jumpRight;
+		vector <totalState> still_jumpLeft;
+
+		vector <totalState> forwards_stillSide;
+		vector <totalState> forwards_runRight;
+		vector <totalState> forwards_runLeft;
+		vector <totalState> forwards_jumpRight;
+		vector <totalState> forwards_jumpLeft;
+
+		vector <totalState> backwards_stillSide;
+		vector <totalState> backwards_runRight;
+		vector <totalState> backwards_runLeft;
+		vector <totalState> backwards_jumpRight;
+		vector <totalState> backwards_jumpLeft;
+
+		vector <totalState> jumping_stillSide;
+		vector <totalState> jumping_jumpRight;
+		vector <totalState> jumping_jumpLeft;
+
+		vector <totalState> jumpingforward_stillSide;
+		vector <totalState> jumpingforward_jumpRight;
+		vector <totalState> jumpingforward_jumpLeft;
+
+		vector <totalState> jumpingBackwards_stillSide;
+		vector <totalState> jumpingBackwards_jumpRight;
+		vector <totalState> jumpingBackwards_jumpLeft;
 	};
 };
