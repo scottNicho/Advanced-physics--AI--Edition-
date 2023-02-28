4ca1b2d37f3869fd78aa4c4451bf5eaf22d36bdc
scottNicho <120742244+scottNicho@users.noreply.github.com>
2023-02-28 13:37:31 +0000

    Update Enemy_AI.h
    Added relativeLeftOr right function to turn to face a target efficiently.

--- Enemy_AI.h
+++ Enemy_AI.h
@@ -6,7 +6,7 @@
 #include"Vector3.h"
 #include<cmath>
 #include<set>
-
+#include"PhysicsObject.h"
 
 
 using namespace std;
@@ -33,16 +33,61 @@ namespace NCL::CSC8503 {
 			agressionLevel = newAgressionLevel;
 		}
 
-		float angleToTarget(GameObject* targetPlayer) {
-			Vector3 targetPosition = targetPlayer->GetTransform().GetPosition();
-			Vector3 enemyPosition = this->GetTransform().GetPosition();
-			Vector3 playerToTarget = (targetPosition) - (enemyPosition);
-			float targetX = targetPosition.x;float targetY = targetPosition.y;
-			float enemyX = enemyPosition.x;float enemyY = enemyPosition.y;
-			Vector3 playerOrientation = this->GetTransform().GetOrientation().ToEuler();
-			float angleBetween = atan2((targetX*enemyY)-(enemyX*targetY),(enemyX*targetX)+(enemyY*targetY));
+		float getAngleObjectTarget( GameObject* target) {
+			Vector3 targetObjectPosition = target->GetTransform().GetPosition();
+			Vector3 objectfacing = (this->GetTransform().GetOrientation() * Vector3 { 0, 0, -1 }).Normalised();
+			Vector3 objectToTarget = ((targetObjectPosition) - (this->GetTransform().GetPosition())).Normalised();
+			float dotProduct = Vector3::Dot(objectfacing, objectToTarget);
+			float angleBetween = acos(dotProduct / ((objectfacing.Length()) * (objectToTarget.Length())));
 			return angleBetween;
 		}
+
+		void faceTarget( GameObject* playerTarget) {
+			Vector3 playerTargetPosition = playerTarget->GetTransform().GetPosition();
+			float angle = getAngleObjectTarget(playerTarget);
+			if (angle <= 0.2) {
+				this->GetPhysicsObject()->ClearForces();
+				this->GetPhysicsObject()->SetAngularVelocity({0,0,0});
+				return;
+			}
+			if (relativeLeftOrRight(playerTarget) == 1) {
+				this->GetPhysicsObject()->AddTorque({ 0,1,0 });
+			}
+			else
+			{
+				this->GetPhysicsObject()->AddTorque({ 0,-1,0 });
+			}
+
+		}
+
+		int relativeLeftOrRight(GameObject* targetPlayer) {
+			Vector3 EnemyPosition = this->GetTransform().GetPosition();
+			Vector3 TargetPosition = targetPlayer->GetTransform().GetPosition();
+			Vector2 EnemyRelativePosition = {  EnemyPosition.x, EnemyPosition.z}; //consider the enemy position as the origin
+			float EnemyPositionGradient = EnemyRelativePosition.y / EnemyRelativePosition.x; // gradient of the cartiesian line going through the normalised direction vector and the origin
+			Vector2 TargetRelativePosition = {TargetPosition.x - EnemyPosition.x,TargetPosition.z - EnemyPosition.z}; // the target augmented in to space relative to the enemy
+			float interceptXPoint = (TargetRelativePosition.y * EnemyRelativePosition.x) / EnemyRelativePosition.y; // solve for the x value when a horisontal line from target intercepts
+			Vector2 iterceptionPoint = {interceptXPoint,TargetRelativePosition.y };
+			Vector2 targetToInterceptPoint = iterceptionPoint - TargetRelativePosition; // get the vector from target to intercep point to get the direction
+			
+			if (targetToInterceptPoint.x > 0 && (EnemyRelativePosition.y>= 0) ) {
+				return 1;
+			}
+			if (targetToInterceptPoint.x > 0 && (EnemyRelativePosition.y < 0)) {
+				return -1;
+			}
+			if (targetToInterceptPoint.x < 0 && (EnemyRelativePosition.y > 0)) {
+				return -1;
+			}
+
+			if (targetToInterceptPoint.x < 0 && (EnemyRelativePosition.y < 0)) {
+				return 1;
+			}
+			/*Vector3 EnemyDirectionVector = ((this->GetTransform().GetOrientation())*Vector3{0,0,-1}).Normalised();
+			return EnemyDirectionVector;*/
+			return 1;
+		}
+
 
 	protected:
 		const  Vector3 centreSafteyCircle = Vector3{0,-20,0};
