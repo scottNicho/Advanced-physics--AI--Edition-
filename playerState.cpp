e510daea78a46ae25014e492e198b8a24cc858d9
scottNicho <120742244+scottNicho@users.noreply.github.com>
2023-03-25 20:56:05 +0000

    Update playerState.cpp

--- playerState.cpp
+++ playerState.cpp
@@ -18,33 +18,35 @@ namespace NCL::CSC8503 {
 		currentState->sidewardsResponse = currentStateSideward::non_side;
 		setInitialPosition();
 		oldPosition = thePlayer->GetTransform().GetPosition();
-		std::map<std::array<int, 2>, totalState, ArrayCompare> totalResponse{
-			{{0, 0}, {still, still_side}},
-            {{0, 1}, {still, run_right}},
-            {{0, 2}, {still, run_left}},
-            {{0, 3}, {still, jumping_right}},
-            {{0, 4}, {still, jumping_left}},
-            {{1, 0}, {forwards, still_side}},
-            {{1, 1}, {forwards, run_right}},
-            {{1, 2}, {forwards, run_left}},
-            {{1, 3}, {forwards, jumping_right}},
-            {{1, 4}, {forwards, jumping_left}},
-            {{2, 0}, {backwards, still_side}},
-            {{2, 1}, {backwards, run_right}},
-            {{2, 2}, {backwards, run_left}},
-            {{2, 3}, {backwards, jumping_right}},
-            {{2, 4}, {backwards, jumping_left}},
-            {{3, 0}, {jumping, still_side}},
-            {{3, 3}, {jumping, jumping_right}},
-            {{3, 4}, {jumping, jumping_left}},
-            {{4, 0}, {jumping_forwards, still_side}},
-            {{4, 3}, {jumping_forwards, jumping_right}},
-            {{4, 4}, {jumping_forwards, jumping_left}},
-            {{5, 0}, {jumping_backwards, still_side}},
-            {{5, 3}, {jumping_backwards, jumping_right}},
-            {{5, 4}, {jumping_backwards, jumping_left}}
+		totalResponse = {
+		 { {0, 0}, {{still, still_side}} },
+		 { {0, 1}, {{still, run_right}} },
+		 { {0, 2}, {{still, run_left}} },
+		 { {0, 3}, {{still, jumping_right}} },
+		 { {0, 4}, {{still, jumping_left}} },
+		 { {1, 0}, {{forwards, still_side}} },
+		 { {1, 1}, {{forwards, run_right}} },
+		 { {1, 2}, {{forwards, run_left}} },
+		 { {1, 3}, {{forwards, jumping_right}} },
+		 { {1, 4}, {{forwards, jumping_left}} },
+		 { {2, 0}, {{backwards, still_side}} },
+		 { {2, 1}, {{backwards, run_right}} },
+		 { {2, 2}, {{backwards, run_left}} },
+		 { {2, 3}, {{backwards, jumping_right}} },
+		 { {2, 4}, {{backwards, jumping_left}} },
+		 { {3, 0}, {{jumping, still_side}} },
+		 { {3, 3}, {{jumping, jumping_right}} },
+		 { {3, 4}, {{jumping, jumping_left}} },
+		 { {4, 0}, {{jumping_forwards, still_side}} },
+		 { {4, 3}, {{jumping_forwards, jumping_right}} },
+		 { {4, 4}, {{jumping_forwards, jumping_left}} },
+		 { {5, 0}, {{jumping_backwards, still_side}} },
+		 { {5, 3}, {{jumping_backwards, jumping_right}} },
+		 { {5, 4}, {{jumping_backwards, jumping_left}} }
 		};
 	}
+
+	
 
 	playerState::~playerState() {
 		delete currentState;
@@ -185,6 +187,19 @@ namespace NCL::CSC8503 {
 			//updateResponse();
 		}
 
+	}
+
+	void playerState::UpdateCurrentState() {
+		if (!playerForwardState || !playerSideState) { return; }
+		currentState->forwardResponse = playerForwardState;
+		currentState->sidewardsResponse = playerSideState;
+	}
+
+	void playerState::setPlayerSpeed(float time) {
+		Vector3 newPosition = playerTrack->GetTransform().GetPosition();
+		float distance = (oldPosition - newPosition).Length();
+		playerSpeed = distance / time;
+		oldPosition = newPosition;
 	}
 
 }
